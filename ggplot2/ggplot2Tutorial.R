0524fa8f31a17f1d54ad4f8ffdfd605bebc344aa
Kevin <kevinyihchyunlu@gmail.com>
2016-09-06 13:53:54 +1000

    finished ggplot2 tutorial

--- ggplot2/ggplot2Tutorial.R
+++ ggplot2/ggplot2Tutorial.R
@@ -210,8 +210,133 @@ p2 + theme_new
 # To plot two variables in a single graph as separate points, with different color depending on which variable it is
 housing.byyear <- aggregate(cbind(Home.Value, Land.Value) ~ Date, data = housing, mean)
 
+# The wrong way
 head(housing.byyear)
 ggplot(housing.byyear,
        aes(x=Date)) +
   geom_line(aes(y=Home.Value), color="red") +
   geom_line(aes(y=Land.Value), color="blue")
+
+# The right way
+library(tidyr)
+
+# use the gather function in tidyr what this function does is:
+# that it gathers multiple columns of data and collapses it into a single
+# column type which each type being a column, the variables that need to be
+# duplicated as a result of collapsing the table are automatically duplicated
+
+# So this function call, gets the house.byyear data frame, and then names the
+# columns for home.land.byyear through the value, and key, then collapses the 
+# Home.Value and Land.Value into a single column
+home.land.byyear <- gather(housing.byyear,
+                           value = "value",
+                           key = "type",
+                           Home.Value, Land.Value)
+head(home.land.byyear)
+
+### PUTTING IT ALL TOGETHER ###
+dat <- read.csv("dataSets/EconomistData.csv")
+str(dat)
+p1 <- ggplot(dat, aes(x=CPI, y=HDI))
+p2 <- p1 + geom_smooth(aes(group=1), 
+                       se = FALSE, 
+                       method = "lm", 
+                       formula = y ~ log(x),
+                       color="red")
+
+p3 <- p2 + 
+  geom_point(shape = 1, size = 4, aes(color=Region)) + 
+  geom_point(shape = 1, size = 4.5, aes(color=Region)) + 
+  geom_point(shape = 1, size = 5, aes(color=Region))
+
+
+pointsToLabel <- c("Russia", "Venezuela", "Iraq", "Myanmar", "Sudan",
+                   "Afghanistan", "Congo", "Greece", "Argentina", "Brazil",
+                   "India", "Italy", "China", "South Africa", "Spane",
+                   "Botswana", "Cape Verde", "Bhutan", "Rwanda", "France",
+                   "United States", "Germany", "Britain", "Barbados", "Norway", "Japan",
+                   "New Zealand", "Singapore")
+
+library(ggrepel)
+?ggrepel
+p4 <- p3 + geom_text_repel(aes(label=Country),
+                     color = "gray20",
+                     data = subset(dat, Country %in% pointsToLabel),
+                     force = 10)
+p4
+
+# converting the region into factors
+dat$Region <- factor(dat$Region,
+                     levels = c("EU W. Europe",
+                                "Americas",
+                                "Asia Pacific",
+                                "East EU Cemt Asia",
+                                "MENA",
+                                "SSA"),
+                     labels = c("OECD",
+                                "Americas",
+                                "Asia &\nOceania",
+                                "Central &\nEastern Europe",
+                                "Middle East &\nnorth Africa",
+                                "Sub-Saharan\nAfrica"))
+
+# access the data object that ggplot has
+p4$data <- dat
+p5 <- p4 +
+  scale_x_continuous(name = "Corruption Perceptions Index, 2011 (10=least corrupt)",
+                     limits = c(.9, 10.5),
+                     breaks = 1:10) +
+  scale_y_continuous(name = "Human Development Index, 2011 (1=Best)",
+                     limits = c(0.2, 1.0),
+                     breaks = seq(0.2, 1.0, by = 0.1)) +
+  scale_color_manual(name = "",
+                     values = c("#24576D",
+                                "#099DD7",
+                                "#28AADC",
+                                "#248E84",
+                                "#F2583F",
+                                "#96503F")) +
+  ggtitle("Corruption and Human development")
+
+library(grid) # for the 'unit' function
+p6 <- p5 +
+   theme_minimal() + # start with a minimal theme and add what we need
+   theme(text = element_text(color = "gray20"),
+         legend.position = c("top"), # position the legend in the upper left 
+         legend.direction = "horizontal",
+         legend.justification = 0.1, # anchor point for legend.position.
+         legend.text = element_text(size = 11, color = "gray10"),
+         axis.text = element_text(face = "italic"),
+         axis.title.x = element_text(vjust = -1), # move title away from axis
+         axis.title.y = element_text(vjust = 2), # move away for axis
+         axis.ticks.y = element_blank(), # element_blank() is how we remove elements
+         axis.line = element_line(color = "gray40", size = 0.5),
+         axis.line.y = element_blank(),
+         panel.grid.major = element_line(color = "gray50", size = 0.5),
+         panel.grid.major.x = element_blank()
+   )
+p6
+
+# get the r squared
+mR2 <- summary(lm(HDI ~ log(CPI), data = dat))$r.squared
+
+png(file = "images/econScatter10.png", width = 800, height = 600)
+p6
+grid.text("Sources: Transparency International; UN Human Development Report",
+          x = .02, y = .03,
+          just = "left",
+          draw = TRUE)
+grid.segments(x0 = 0.81, x1 = 0.825,
+              y0 = 0.90, y1 = 0.90,
+              gp = gpar(col = "red"),
+              draw = TRUE)
+grid.text(paste0("R² = ",
+                 as.integer(mR2*100),
+                 "%"),
+          x = 0.835, y = 0.90,
+          gp = gpar(col = "gray20"),
+          draw = TRUE,
+          just = "left")
+
+dev.off()
+p6
